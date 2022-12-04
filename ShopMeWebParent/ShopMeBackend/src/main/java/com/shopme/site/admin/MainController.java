fe9b9b41903d04fbb225edf27d161add11968d90
Divyansh <goswami7917@gmail.com>
2022-12-04 19:15:13 +0530

    initial commit

---
+++ ShopMeWebParent/ShopMeBackend/src/main/java/com/shopme/site/admin/MainController.java
@@ -0,0 +1,12 @@
+package com.shopme.site.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping
+    public String viewAdminPage(){
+        return "index";
+    }
+}
